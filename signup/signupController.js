110e9290c896ef6ce0fe85e1600abb6c959e91b8
Juan Manuel <juanmanuel31@hotmail.com>
2023-03-12 23:07:59 +0100

    Refactorización del formulario y validación

--- signup/signupController.js
+++ signup/signupController.js
@@ -2,23 +2,39 @@ export function signupController(signupElement) {
   signupElement.addEventListener("submit", (event) => {
     event.preventDefault();
 
+    if (isFormValid(signupElement)) {
+      // crear usuario
+    }
+  });
+
+  function isPasswordValid(signupElement) {
+    const mailRegExp = new RegExp(
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    );
+    const emailElement = signupElement.querySelector("#username");
+
+    if (!mailRegExp.test(emailElement.value)) {
+      alert("El email no está bien escrito");
+      return false;
+    }
+
+    return true
+  }
+
+  function isEmailValid(signupElement) {
     const passwordElement = signupElement.querySelector("#password");
     const passwordConfirmElement =
       signupElement.querySelector("#passwordConfirm");
 
     if (passwordElement.value !== passwordConfirmElement.value) {
       alert("Las contraseñas no son iguales");
-    }
-    const mailRegExp = new RegExp(
-      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-    );
-    const emailElement = signupElement.querySelector("#username");
-    if (!mailRegExp.test(emailElement.value)) {
-      alert("El email no está bien escrito");
+      return false
     }
 
-    if (isEmailValid() && arePasswordsValid()) {
-      // creamos el usuario
-    }
-  });
+    return true;
+  }
+}
+
+function isFormValid(signupElement) {
+  return isEmailValid(signupElement) && isPasswordValid(signupElement);
 }
