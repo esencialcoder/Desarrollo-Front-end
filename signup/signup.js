4413f542bc3995314b441386d08874d8ace4c752
Juan Manuel <juanmanuel31@hotmail.com>
2023-03-13 13:02:26 +0100

    Crear usuario a través de sparrest

---
+++ signup/signup.js
@@ -0,0 +1,22 @@
+export async function createUser(email, password) {
+  // cosntruir un objeto con email y passport siguiendo en el endpoint /auth/register
+  const user = {
+    username: email,
+    password: password,
+  };
+
+  // consumir el api sparrest utilizando un POST y enviando los datos que ha introducido el usuario
+  const response = await fetch("http://localhost:8000/auth/register", {
+    method: "POST",
+    body: JSON.stringify(user),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await response.json();
+
+  return data;
+
+  // gestionar la respuesta
+}
--- signup/signupController.js
+++ signup/signupController.js
@@ -1,19 +1,33 @@
+import { createUser } from "./signup.js";
+
 export function signupController(signupElement) {
-  signupElement.addEventListener("submit", (event) => {
+  signupElement.addEventListener("submit", async (event) => {
     event.preventDefault();
 
-    if (isFormValid(signupElement)) {
-      // crear usuario
+    const emailElement = signupElement.querySelector("#username");
+    const passwordElement = signupElement.querySelector("#password");
+    const passwordConfirmElement =
+      signupElement.querySelector("#passwordConfirm");
+
+    if (
+      isEmailValid(emailElement.value) &&
+      isPasswordValid(passwordElement.value, passwordConfirmElement.value)
+    ) {
+      try {
+        await createUser(emailElement.value, passwordElement.value)
+        alert('Usuario creado correctamente')
+      } catch (error) {
+        alert('No ha podido crearse el usuario')
+      }
     }
   });
 
-  function isPasswordValid(signupElement) {
+  function isEmailValid(email) {
     const mailRegExp = new RegExp(
       /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
     );
-    const emailElement = signupElement.querySelector("#username");
 
-    if (!mailRegExp.test(emailElement.value)) {
+    if (!mailRegExp.test(email)) {
       alert("El email no está bien escrito");
       return false;
     }
@@ -21,21 +35,12 @@ export function signupController(signupElement) {
     return true;
   }
 
-  function isEmailValid(signupElement) {
-    const passwordElement = signupElement.querySelector("#password");
-    const passwordConfirmElement =
-      signupElement.querySelector("#passwordConfirm");
-
-    if (passwordElement.value !== passwordConfirmElement.value) {
+  function isPasswordValid(password, passwordConfirmation) {
+    if (password !== passwordConfirmation) {
       alert("Las contraseñas no son iguales");
       return false;
     }
 
     return true;
   }
-  
-  function isFormValid(signupElement) {
-    return isEmailValid(signupElement) && isPasswordValid(signupElement);
-  }
 }
-
